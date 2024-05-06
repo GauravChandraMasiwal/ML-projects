a2be29be11e87fd4f4a3b2727a7e765bafeb8ffd
GauravChandraMasiwal <chandra.gaurav2018@gmail.com>
2024-05-06 17:30:10 +0530

    Predict pipeline added with app.py

--- src/utils.py
+++ src/utils.py
@@ -55,7 +55,7 @@ def load_object(file_path):
     try:
         
         with open(file_path, "rb") as file_obj:
-            pickle.load(file_obj)
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
